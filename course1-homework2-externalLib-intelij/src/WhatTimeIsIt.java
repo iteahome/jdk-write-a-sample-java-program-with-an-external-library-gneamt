04997849218fb2b2190dd9850d02a664b6214b54
george neamt <georgeneamt2004@yahoo.com>
2019-11-30 22:27:14 +0200

    added intelij project

---
+++ course1-homework2-externalLib-intelij/src/WhatTimeIsIt.java
@@ -0,0 +1,10 @@
+import org.joda.time.*;
+
+class WhatTimeIsIt{
+    public static void main(String args[]){
+        System.out.println("Este ora:");
+        LocalTime localtime = new LocalTime();
+        System.out.println(localtime.toString());
+    }
+
+}
